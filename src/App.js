a4fa3ff3f20d2ffe601ddd0199d1f9fa6ae0c71c
adospinoiu <alex.dospinoiu@gmail.com>
2021-07-01 05:22:42 -0700

    Added sub-components into main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+//Import Sub-Components
+import Header from './components/Header';
+import About from './components/About';
+import Profile from './components/Profile';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router Tutorial</h1>
+
+      <Header />
+      <About />
+      <Profile />
     </div>
   );
 }
