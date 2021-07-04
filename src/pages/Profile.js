f55247433428154ec41a2f9007acf7493c795920
adospinoiu <alex.dospinoiu@gmail.com>
2021-07-04 07:11:03 -0700

    Created routes for the viewprofile and edit profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Switch, Link } from "react-router-dom";
 
 function Profile() {
     return (
+        <>
         <h1>Profile</h1>
+        
+        <ul>
+            <li>
+                <Link>View Profile</Link>
+            </li>
+            <li>
+                <Link>Edit Profile</Link>
+            </li>
+        </ul>
+        
+        <Switch>
+            <Route path='/viewprofile' />
+            <Route path='/editprofile' />
+        </Switch>
+        </>
     )
 }
 
