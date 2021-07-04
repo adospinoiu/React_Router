dffbe93fd8034f2e7768d7c496d44c872b63a988
adospinoiu <alex.dospinoiu@gmail.com>
2021-07-04 07:27:45 -0700

    All the routes and paths work properly

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,23 +1,28 @@
 import React from 'react'
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link, useRouteMatch } from "react-router-dom";
+
+import EditProfile from '../components/EditProfile';
+import ViewProfile from '../components/ViewProfile';
 
 function Profile() {
+    const { path, url } = useRouteMatch();
+    
     return (
         <>
         <h1>Profile</h1>
         
         <ul>
             <li>
-                <Link>View Profile</Link>
+                <Link to={`${url}/viewprofile`}>View Profile</Link>
             </li>
             <li>
-                <Link>Edit Profile</Link>
+                <Link to={`${url}/editprofile`}>Edit Profile</Link>
             </li>
         </ul>
         
         <Switch>
-            <Route path='/viewprofile' />
-            <Route path='/editprofile' />
+            <Route path={`${path}/viewprofile`} component={ViewProfile} />
+            <Route path={`${path}/editprofile`} component={EditProfile} />
         </Switch>
         </>
     )
