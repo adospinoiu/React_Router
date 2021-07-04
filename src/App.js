e10b22645c73f05d0b94ca7077762ab04254bfc6
adospinoiu <alex.dospinoiu@gmail.com>
2021-07-04 06:33:33 -0700

    Added Button to test useState

--- src/App.js
+++ src/App.js
@@ -9,20 +9,26 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Post from './pages/Post';
 import NotFound from './pages/NotFound';
+import { useState } from 'react';
 
 function App() {
+  const [login, setLogin] = useState(false)
+
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+
+        <button>Login</button>
+
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path="/about" component={About} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/post/:id" component={Post} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
-      <Switch>
-        <Route path='/' component={Home} exact/>
-        <Route path="/about" component={About} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/post/:id" component={Post} />
-        <Route component={NotFound} />
-      </Switch>  
     </BrowserRouter>
 
   );
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 function Post() {
     //This will let you get information from the URL
     const { id } = useParams();
-    
+
+    const query = new URLSearchParams(useLocation().search)
+
     return (
-        <h2>ID IS: { id } </h2>
+        <>
+            <h2>ID IS: {id} </h2>
+            <h2>{query.get('first')}</h2>
+            <h2>{query.get('last')}</h2>
+        </>
     )
 }
 
